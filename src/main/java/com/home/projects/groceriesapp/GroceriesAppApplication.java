853fda5b204705cc01b79c16e17c780e0714b694
Hogea Andrei <hogik00@gmail.com>
2023-09-23 18:51:41 +0300

    first commit

---
+++ src/main/java/com/home/projects/groceriesapp/GroceriesAppApplication.java
@@ -0,0 +1,13 @@
+package com.home.projects.groceriesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceriesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/projects/groceriesapp/GroceriesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.projects.groceriesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
